4b1d1a35d98fdbc5952edc793a4e65835ce057b8
AkCzx <nathan19952008@gmail.com>
2021-02-23 17:13:45 +0800

    UI Done

---
+++ tests/NoteService.test.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const { TestScheduler } = require("jest");
+const path = require("path");
+const file = __dirname + path.sep + "test.json";
+
+//Before Each
+describe("Note Service should handle files properly", () => {
+  beforeEach((done) => {
+    done();
+  });
+});
