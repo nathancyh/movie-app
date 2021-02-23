5283ebf7e5f11147814ebee09e7627b7fdd4c495
AkCzx <nathan19952008@gmail.com>
2021-02-23 18:36:38 +0800

    End of day

--- tests/NoteService.test.js
+++ tests/NoteService.test.js
@@ -1,11 +1,27 @@
 const fs = require("fs");
-const { TestScheduler } = require("jest");
+const NoteService = require("../noteService");
 const path = require("path");
+const { TestScheduler } = require("jest");
 const file = __dirname + path.sep + "test.json";
 
 //Before Each
 describe("Note Service should handle files properly", () => {
   beforeEach((done) => {
-    done();
+    fs.unlink(file, (err) => {
+      if (err) {
+        console.log(err);
+        this.noteService = new NoteService(file);
+      }
+      this.noteService = new NoteService(file);
+      done();
+    });
+  });
+
+  test("At first the note should be empty", () => {
+    return this.noteService.list().then((notes) => expect(notes).toEqual({}));
+  });
+
+  test("At first the note should be empty", () => {
+    return this.noteService.list().then((notes) => expect(notes).toEqual({}));
   });
 });
