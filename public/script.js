cb0e91bd7f384ec656f09fde1a782bbcf4764eea
nathan19952008@gmail.com <nathan19952008@gmail.com>
2021-03-16 23:41:00 +0800

    fixed post

--- public/script.js
+++ public/script.js
@@ -1,25 +1,26 @@
 "use strict";
 
-$(function(){
-    console.log('document running')
+$(function () {
+  console.log("document running");
 
-    $("#post").click((e) => {
-        e.preventDefault();
-        let data = $("#text-area").val();
-        console.log($("#text-area").val())
-        console.log(window.location.search)
-        $.ajax({
-            type:"POST",
-            url: "/movie/:movieId",
-            dataType: "text",
-            data: {note: data},
-            success: function () {
-                console.log("post success");
-              },
-            }).done(
-              setTimeout(() => {
-                // window.location.reload();
-              }, 200)
-            );
-    });
-})
+  $("#post").click((e) => {
+    e.preventDefault();
+    let data = $("#text-area").val();
+    //getting :id from /movie/:id
+    let movieid = window.location.pathname.slice(7);
+    console.log(movieid, data);
+    $.ajax({
+      type: "POST",
+      url: `/movie/${movieid}`,
+      dataType: "text",
+      data: { note: data },
+      success: function () {
+        console.log("post success");
+      },
+    }).done(
+      setTimeout(() => {
+        window.location.reload();
+      }, 200)
+    );
+  });
+});
--- routers/movieRouter.js
+++ routers/movieRouter.js
@@ -1,65 +1,66 @@
 module.exports = (express) => {
-    console.log("router running")
-    const router = express.Router();
-    const axios = require('axios');
+  console.log("router running");
+  const router = express.Router();
+  const axios = require("axios");
 
-    // Knex Setup
-    const knexConfig = require("../knexfile").development;
-    const knex = require("knex")(knexConfig);
+  // Knex Setup
+  const knexConfig = require("../knexfile").development;
+  const knex = require("knex")(knexConfig);
 
-  
-    const MovieService = require('../services/movieService') ;
-    const movieService = new MovieService(knex);
+  const MovieService = require("../services/movieService");
+  const movieService = new MovieService(knex);
 
-    router.get('/:movieId', function (req, res) {
-        function getMovieData() {
-            return axios
-                .get(`https://api.themoviedb.org/3/movie/${req.params.movieId}?api_key=d3fd18f172ad640f103d9cfa9fb37451`)
-                .then((info) => {
-                    return info.data;
-                })
-                .catch((err) => {
-                    console.log("axios get err", err);
-                });
-        }
+  router.get("/:movieId", function (req, res) {
+    function getMovieData() {
+      return axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${req.params.movieId}?api_key=d3fd18f172ad640f103d9cfa9fb37451`
+        )
+        .then((info) => {
+          return info.data;
+        })
+        .catch((err) => {
+          console.log("axios get err", err);
+        });
+    }
 
-        function getMovieReview() {
-            return movieService.list(req.params.movieId)
-            .then((data) => {
-                // console.log("get review", data)
-                return data
-            })
-            .catch((err) => res.status(500).json(err));
-        }
-
-        Promise.all([getMovieData(), getMovieReview()])
-        .then(function(results) {
-
-            const data = results[0];
-            const movieReview = results[1];
-
-            // console.log("movieReview", movieReview)
-
-            res.render('review', {
-                        poster: `https://image.tmdb.org/t/p/w300${data.poster_path}`,
-                        title: data.title,
-                        genres: data.genres[0].name,
-                        releaseDate: data.release_date.slice(0, 4),
-                        overview: data.overview,
-                        reviewArr: movieReview
-                    })
+    function getMovieReview() {
+      return movieService
+        .list(req.params.movieId)
+        .then((data) => {
+          // console.log("get review", data)
+          return data;
         })
         .catch((err) => res.status(500).json(err));
-    })
+    }
 
-    router.post('/:movieId', function(req, res) {
-        console.log("post review")
-        console.log(req.body)
-        return movieService.add(req.params.movieId, 1, req.body.note)
-        .then(() => res.redirect('/'))
-        .catch((err) => res.status(500).json(err));
-    })
+    Promise.all([getMovieData(), getMovieReview()])
+      .then(function (results) {
+        const data = results[0];
+        const movieReview = results[1];
 
-    return router;
+        // console.log("movieReview", movieReview)
 
-}
+        res.render("review", {
+          poster: `https://image.tmdb.org/t/p/w300${data.poster_path}`,
+          title: data.title,
+          genres: data.genres[0].name,
+          releaseDate: data.release_date.slice(0, 4),
+          overview: data.overview,
+          movieid: data.id,
+          reviewArr: movieReview,
+        });
+      })
+      .catch((err) => res.status(500).json(err));
+  });
+
+  router.post("/:movieId", function (req, res) {
+    console.log("post review");
+    return movieService
+      .add(req.params.movieId, 1, req.body.note)
+      .then(() => res.redirect("/"))
+      .catch((err) => res.status(500).json(err));
+  });
+
+  return router;
+};
