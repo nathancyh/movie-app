95f670dbba9cd00a0684f61e04a68d8fdbfa64ac
AkCzx <nathan19952008@gmail.com>
2021-03-22 22:08:11 +0800

    added switch

--- routers/searchRouter.js
+++ routers/searchRouter.js
@@ -5,13 +5,34 @@ module.exports = (express) => {
   const axios = require("axios");
 
   router.route("/").get(getSearch);
-  router.route("/:query").get(getSearchQuery);
+  // router.route("?order").get(getSearchQuery);
 
   function getSearch(req, res) {
+    //default popular
+    console.log("querystring");
+    console.log(req.query);
+    let sortOption = "popularity.desc";
+    // let sortOption = "vote_count.desc";
+
+    switch (req.query) {
+      case "popularity.desc":
+        sortOption = "popularity.desc";
+        break;
+
+      case "release_date.desc":
+        sortOption = "release_date.desc";
+        break;
+
+      case "revenue.desc":
+        sortOption = "release_date.desc";
+        break;
+    }
+
     return axios
       .get(
-        // `https://api.themoviedb.org/3/discover/movie?api_key=f22e6ce68f5e5002e71c20bcba477e7d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28`
-        `https://api.themoviedb.org/3/discover/movie?api_key=f22e6ce68f5e5002e71c20bcba477e7d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1`
+        // `https://api.themoviedb.org/3/discover/movie?api_key=f22e6ce68f5e5002e71c20bcba477e7d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1`
+
+        `https://api.themoviedb.org/3/discover/movie?api_key=f22e6ce68f5e5002e71c20bcba477e7d&language=en-US&sort_by=${sortOption}&include_adult=false&include_video=true&page=1`
       )
       .then((info) => {
         // res.send("hello from searchRouter");
@@ -41,3 +62,14 @@ module.exports = (express) => {
 };
 
 // "https://api.themoviedb.org/3/discover/movie?api_key=f22e6ce68f5e5002e71c20bcba477e7d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28"
+
+//sort_by=
+//release_date.asc
+//release_date.desc
+//revenue.asc
+//revenue.desc
+//vote_average.asc
+//vote_average.desc
+//vote_count.asc
+//vote_count.desc
+// ▲ ▼
