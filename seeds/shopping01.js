9b3bd1da2460cb8f2467870f4407aa2195a36f0d
missjulietran <76934982+missjulietran@users.noreply.github.com>
2021-03-26 19:38:05 +0800

    review edit page completed, nav bar login dropdown works, removed logout btn

---
+++ seeds/shopping01.js
@@ -0,0 +1,18 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex("shopping")
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex("shopping").insert([
+        {
+          id: 1,
+          title: "White hoodie",
+          price: "10.99",
+          image: "product-1.jpeg",
+        },
+        { id: 2, colName: "rowValue2" },
+        { id: 3, colName: "rowValue3" },
+      ]);
+    });
+};
